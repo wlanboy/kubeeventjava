fd889023354c4cae06d016ef3793580977eb86b6
wlanboy <wlanboy@github.com>
2026-05-07 20:54:34 +0200

    Added flicker daemonset, statefulset

--- testjob/delete.sh
+++ testjob/delete.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 kubectl delete -f test-daemonset.yml --ignore-not-found
+kubectl delete -f test-daemonset-flicker.yml --ignore-not-found
 kubectl delete -f test-statefulset.yml --ignore-not-found
+kubectl delete -f test-statefulset-flicker.yml --ignore-not-found
 kubectl delete -f test-cronjob.yml --ignore-not-found
 kubectl delete -f test-cronjob-fail.yml --ignore-not-found
 echo "Alle Test-Ressourcen geloescht."
